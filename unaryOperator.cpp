2148487576cb6cc85ac87f7409e4df3ade3801f2
Shawon <shawonh986@gmail.com>
2025-03-24 01:12:35 +0800

    unary operator

---
+++ unaryOperator.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int k = 4;
+    int m = ++k;
+
+    cout<<"k = "<<k<<endl;
+    cout<<"m = "<<m<<endl;
+
+    int x = 3;
+    int y = x++ ;
+
+    cout<<"x = "<<x<<endl;
+    cout<<"y = "<<y<<endl;
+
+    int i = 2;
+    int j = --i;
+
+    cout<<"i = "<<i<<endl;
+    cout<<"j = "<<j<<endl;
+
+    int o = 5;
+    int p = o--;
+
+    cout<<"o = "<<o<<endl;
+    cout<<"p = "<<p<<endl;
+
+
+
+
+
+
+    return 0;
+
+}
