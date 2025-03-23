22864e2f214819bfa5fb2af7da016ff35090ea6d
Shawon <shawonh986@gmail.com>
2025-03-23 23:41:07 +0800

    arithmetic operator

---
+++ arithmeticOperator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1 =10,num2 =7;
+
+    int sum = num1 + num2 ;
+    cout<<"Sum is = "<<sum<<endl;
+
+    int sub = num1 - num2;
+    cout<<"Sub is = "<<sub<<endl;
+
+    int mult = num1 * num2;
+    cout<<"Mult is = "<<mult<<endl;
+
+    double div = (float) num1 / num2;
+    cout<<"Div is = "<<div<<endl;
+
+    int mod = num1 % num2;
+    cout<<"Mod is = "<<mod;
+
+    return 0;
+    
+}
