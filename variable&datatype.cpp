13bc500c94f16612707e51579f522eca916405cd
Shawon <shawonh986@gmail.com>
2025-03-23 22:52:48 +0800

    variable and data type

---
+++ variable&datatype.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+    int num1, num2;
+
+    num1 = 12;
+    num2 = 34;
+
+    cout << num1+num2 <<endl;
+
+
+    int num3 = 12, num4 = 34;
+
+    cout << "The sum is : " << num3 + num4 <<endl ;
+
+    float number1,number2 ;
+    number1 = 23.54;
+    number2 = 21.56;
+
+    cout<< number1 - number2 <<endl;
+
+    double num5 = 23.3242, num6 = 34.34523;
+    cout << num5 * num6 <<endl;
+
+    char ch = 'a';
+    cout << ch;
+
+    return 0;
+}
