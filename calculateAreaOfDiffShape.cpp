648da710bcec941baad5cfb0b9ec33057b4f4ce3
Shawon <shawonh986@gmail.com>
2025-03-24 00:32:27 +0800

    area of trapezium

--- calculateAreaOfDiffShape.cpp
+++ calculateAreaOfDiffShape.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
 
-    //area of triangle
+    //area of Triangle
 
     double base,height;
 
@@ -12,9 +12,26 @@ int main(){
     cout<<"Input Height value : ";
     cin>>height;
 
-    double area ;
-    area = (double) 1/2 * (base * height);
-    cout<<"Area of a Triangle is : "<<area;
+    double triangleArea ;
+    triangleArea = (double) 1/2 * (base * height);
+    cout<<"Area of a Triangle is : "<<triangleArea<<endl;
+
+    //area of Trapezium
+
+    double a,b,h;
+
+    cout<<"Input value of a : ";
+    cin>>a;
+    cout<<"Input value of b : ";
+    cin>>b;
+    cout<<"Input value of h : ";
+    cin>>h;
+
+    double trapeziumArea;
+    trapeziumArea = (double)1/2 * (a+b) * h;
+    cout<<"Area of a Trapezium is : "<<trapeziumArea<<endl;
+    
+
 
     return 0;
 }
