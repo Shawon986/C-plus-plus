d1c534b5fac27e6b1f28140a321341d2dd4b17bc
Shawon <shawonh986@gmail.com>
2025-03-28 00:04:59 +0800

    total prime numbers count

--- sumOfPrimeNumber.cpp
+++ sumOfPrimeNumber.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int i,count =0,number;
+    int i,count =0,number,numberOfPrimeNumbers = 0;
 
     for(number=1; number<=100; number++){
         count = 0;
@@ -17,9 +17,11 @@ int main() {
             }
         }
         if(count == 0){
-            cout<<number<<""<<endl;
+            cout<<number<<" ";
+            numberOfPrimeNumbers++ ;
         }
     }
+    cout<<"\nNumber of Prime Numbers = "<<numberOfPrimeNumbers;
 
     return 0;
 }
