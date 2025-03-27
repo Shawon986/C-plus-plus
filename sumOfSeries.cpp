34560278e123965d5e100273f047106fee10535f
Shawon <shawonh986@gmail.com>
2025-03-28 03:42:32 +0800

    sum off odd numbers

--- sumOfSeries.cpp
+++ sumOfSeries.cpp
@@ -2,27 +2,39 @@
 using namespace std;
 
 int main() {
-    int n ,sum=0;
-    cout<<"Type a number : ";
+    int n ,sumOfN=0;
+    cout<<"Type last number : ";
     cin>>n;
 
     for(int i = 1; i<=n; i++){
-        sum = sum+i;
+        sumOfN = sumOfN+i;
     }
-    cout<<"Sum = "<<sum<<endl;
+    cout<<"Sum = "<<sumOfN<<endl;
 
 
     //sum of even numbers
 
-    int m, sum = 0;
+    int m, sumOfM = 0;
 
     cout<<"Type last number : ";
     cin>>m;
 
     for(int i = 2; i<=m; i+=2){
-        sum = sum+i;
+        sumOfM = sumOfM+i;
     }
-    cout<<"Sum = "<<sum<<endl;
+    cout<<"Sum = "<<sumOfM<<endl;
+
+    //sum off odd numbers
+
+    int p, sumOfP = 0;
+
+    cout<<"Type last number : ";
+    cin>>p;
+
+    for(int i = 1; i<=p; i+=2){
+        sumOfP = sumOfP+i;
+    }
+    cout<<"Sum = "<<sumOfP<<endl;
 
     
     return 0;
