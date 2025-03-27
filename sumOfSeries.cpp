a73a5f3db04c8d541a815ee7d95ce74837d058ae
Shawon <shawonh986@gmail.com>
2025-03-28 03:37:56 +0800

    sum of even numbers

--- sumOfSeries.cpp
+++ sumOfSeries.cpp
@@ -9,7 +9,20 @@ int main() {
     for(int i = 1; i<=n; i++){
         sum = sum+i;
     }
-    cout<<"Sum = "<<sum;
+    cout<<"Sum = "<<sum<<endl;
+
+
+    //sum of even numbers
+
+    int m, sum = 0;
+
+    cout<<"Type last number : ";
+    cin>>m;
+
+    for(int i = 2; i<=m; i+=2){
+        sum = sum+i;
+    }
+    cout<<"Sum = "<<sum<<endl;
 
     
     return 0;
