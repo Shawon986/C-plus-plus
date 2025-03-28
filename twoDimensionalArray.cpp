b783f119080871984ddbbb9b9718b82f47b557f7
Shawon <shawonh986@gmail.com>
2025-03-28 22:15:48 +0800

    two dimensional array

---
+++ twoDimensionalArray.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int array[2][3] = {
+        {10,20,30},
+        {40,50,60}
+    };
+
+    for(int row = 0; row<2; row++){
+        for(int col = 0; col<3; col++){
+            cout<<array[row][col]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
