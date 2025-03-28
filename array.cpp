492ecb4bc91c71ab7e5ece18cec8a3c962c4c927
Shawon <shawonh986@gmail.com>
2025-03-28 20:20:03 +0800

    user input for array using loop

--- array.cpp
+++ array.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int array[] = {20,30,40,50,60,70,80};
 
-    cout<<array[3];
+    cout<<array[3]<<endl;
 
     for(int i =0; i<=6; i++){
         cout<<array[i];
@@ -25,6 +25,20 @@ int main() {
     cout<<array2[2]<<endl;
     cout<<array2[3]<<endl;
     cout<<array2[4]<<endl;
+
+
+    int array3[5];
+
+    cout<<"Type 5 numbers : ";
+    for(int n = 0; n<5; n++){
+        cin>>array3[n];
+    }
+
+    cout<<"Numbers are : ";
+
+    for(int i = 0; i<5; i++){
+        cout<<" "<<array3[i];
+    }
 
 
     return 0;
