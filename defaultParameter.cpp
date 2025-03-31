47a5a184cbbc9ec39bb9fb68a761b8cf0c50be74
Shawon <shawonh986@gmail.com>
2025-04-01 02:08:44 +0800

    calculate square using parameter

--- defaultParameter.cpp
+++ defaultParameter.cpp
@@ -6,9 +6,18 @@ void display(int a =10, int b=20){  //we can set default value in the parameter
     cout<<b<<endl;
 }
 
+void multiplication(int n){
+    int mult = n*n;
+    cout<<"Square of "<<n<<" = "<<mult<<endl;
+}
+
 int main() {
     display();
     display(40,50);  //it will replace the value of the parameter
+    multiplication(5);
+    multiplication(6);
+    multiplication(6);
+    multiplication(7);
     
     return 0;
 }
