bbf4d883b8a574a4cae411651729c203360af894
Shawon <shawonh986@gmail.com>
2025-04-01 01:13:12 +0800

    sum by pointer

--- pointer.cpp
+++ pointer.cpp
@@ -8,7 +8,18 @@ int main() {
 
     cout<<*p<<endl;     //for printing the value of x variable // *p is a pointer//pointer stotres the address of a variable, and it also like a variable
     cout<<&x<<endl;       //for printing the address of x
-    cout<<p;        
+    cout<<p<<endl;
+    
+    
+    int number1 =10, number2 =20;
+
+    int *p1,*p2;
+
+    p1 = &number1;
+    p2 = &number2;
+
+    int sum = *p1+*p2;
+    cout<<sum;
     
     return 0;
 }
