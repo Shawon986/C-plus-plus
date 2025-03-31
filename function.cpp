e5d1b6a00579ec6d4e160c44ba625bb9498b6249
Shawon <shawonh986@gmail.com>
2025-04-01 01:35:56 +0800

    use prototype of a function

--- function.cpp
+++ function.cpp
@@ -6,10 +6,17 @@ void addition(int a, int b){
     cout<<sum<<endl;
 
 }
+void subtraction(int,int); //if we want to decleare function after the main function we have to decleare a prototype of the function like this.
 
 int main() {
-    
+
     addition(10,20);
+    subtraction(20,10);
     
     return 0;
+}
+
+void subtraction(int a, int b){
+    int sub = a-b;
+    cout<<sub<<endl;
 }
