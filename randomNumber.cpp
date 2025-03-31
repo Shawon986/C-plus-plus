4db05250554c0c048f87f6e06fc4156ec6c27f18
Shawon <shawonh986@gmail.com>
2025-04-01 02:14:57 +0800

    printing random number

---
+++ randomNumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    for(int i =1; i<=5; i++){
+        int randomNumber = rand();
+        cout<<"Random number = "<<randomNumber<<endl;
+    }
+
+
+   
+    
+    return 0;
+}
